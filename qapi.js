f93280ada7456670075597e598c0ab1fda827884
arnorhs <arnorhs@gmail.com>
2020-11-09 10:29:15 +0000

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa919292c4ef2001e32065a",
+				"5fa919292c4ef2001e32065c"
+			]
+		},
+		"5fa919292c4ef2001e32065a": {
+			"id": "5fa919292c4ef2001e32065a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa919292c4ef2001e32065c": {
+			"id": "5fa919292c4ef2001e32065c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,140 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Icon, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, StackItem, Stack, Section } from "@quarkly/components";
+import { MdArrowDownward } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1510125594188-5afc74c8cc43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text color="--lightD2" letter-spacing="1px" text-transform="uppercase" margin="0">
+						Best
+					</Text>
+					<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0">
+						Við erum töff ok
+					</Text>
+				</StackItem>
+			</Stack>
+			<Box text-align="center" margin="96px 0 0 0">
+				<Text margin="8px 0" text-transform="uppercase">
+					Who we are
+				</Text>
+				<Icon category="md" margin="0 auto" icon={MdArrowDownward} />
+			</Box>
+		</Section>
+		<Section padding="64px 0" sm-padding="40px 0" font="--base" color="--dark">
+			<Text
+				as="h1"
+				font="--headline1"
+				md-font="--headline2"
+				max-width="520px"
+				margin="0 auto 5px auto"
+				text-align="center"
+			>
+				Sjáðu bara
+			</Text>
+			<Text
+				as="p"
+				color="--grey"
+				max-width="520px"
+				margin="0 auto"
+				text-align="center"
+				font="--lead"
+			>
+				ógissla töff
+			</Text>
+			<Stack margin-top="40px">
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=582&q=80) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						I'm CEO bitch
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Mundi B Sig
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						Frank sappa í svampfrakka. Frank sappa í svampfrakka. Frank sappa í svampfrakka. Frank sappa í svampfrakka. Frank sappa í svampfrakka.{" "}
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1546672741-d327539d5f13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Lead dev
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Nóri H Sig
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						Frank sappa í svampfrakka. Frank sappa í svampfrakka. Frank sappa í svampfrakka. Frank sappa í svampfrakka. Frank sappa í svampfrakka. Frank sappa í svampfrakka.{" "}
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						PM
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Emma
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						Frank sappa í svampfrakka. Frank sappa í svampfrakka. Frank sappa í svampfrakka. Frank sappa í svampfrakka.{" "}
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Dev
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Haffi haff
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
